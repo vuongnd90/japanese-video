0b03af8f8d89810386ea64194c1a35a8a1c258ed
Vuong ND <vuongnd90@gmail.com>
2018-05-04 14:06:46 +0900

    change database config to use pgsql

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
 return [
 
     /*
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],//env('DB_HOST', '127.0.0.1'),
+            'port' => $DATABASE_URL["port"],//env('DB_PORT', '5432'),
+            'database' => ltrim($DATABASE_URL["path"], "/"),//env('DB_DATABASE', 'forge'),
+            'username' => $DATABASE_URL["user"],//env('DB_USERNAME', 'forge'),
+            'password' => $DATABASE_URL["pass"],//env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
